dff49d3b9170285cbeacede66051e4cc9339cd16
Monge Camacho <henrymonge@gmail.com>
2025-07-14 17:13:48 -0400

    updated defs

--- modules/defs.py
+++ modules/defs.py
@@ -75,7 +75,7 @@ def writeInputFileHadspec(params,outdir):
 def setMonomialIds(params):
   params['Monomial_ids']=''
   for m in params['Monomials'].keys():
-      if 'TWO_FLAVOR_EOPREC' in m:
+      if 'TWO_FLAVOR_EOPREC' in m and 'EOPREC_OO' not in m:
         params['Monomial_ids']+='\t\t\t\t\t<elem>'+params['Monomials'][m]['ID']+'_ee</elem>\n'
         params['Monomial_ids']+='\t\t\t\t\t<elem>'+params['Monomials'][m]['ID']+'_oo</elem>\n'
       elif 'TWO_FLAVOR_EOPREC_OO' in m:
