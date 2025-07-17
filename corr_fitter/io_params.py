58ef51880212f6bda721a607c3b320709eb73c03
Monge Camacho <henrymonge@gmail.com>
2025-07-17 08:23:32 -0700

    updated fitter

--- corr_fitter/io_params.py
+++ corr_fitter/io_params.py
@@ -4,7 +4,7 @@ from matplotlib.ticker import MultipleLocator, FormatStrFormatter,FuncFormatter
 
 ##################PLOT DEFAULTS###############################
 rcParams={'text.latex.preamble':r'\usepackage{amsmath} \usepackage{amssymb} \usepackage{slashed}',
-          'text.usetex':True}
+          'text.usetex':False}
         
 plt_rcParam={'axes.linewidth':2.5, 'font.size':25, 'font.weight':1000,'legend.fontsize':25,
              'legend.markerscale':1.5,'grid.linewidth':1.5,'lines.linewidth':2.5,'axes.labelsize':30}
